739fbab84f65c18fcf7f37d31664f55a3afb7175
as1992ale <57945367+as1992ale@users.noreply.github.com>
2026-03-10 20:47:53 +0300

    Update ConfigServerApplication.java

--- src/main/java/org/example/configserver/ConfigServerApplication.java
+++ src/main/java/org/example/configserver/ConfigServerApplication.java
@@ -3,11 +3,9 @@ package org.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableConfigServer // <-- Включаем режим Config Server
-@EnableDiscoveryClient // <-- Регистрируемся в Eureka
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
